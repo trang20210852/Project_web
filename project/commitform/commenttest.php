f65f7d5b582d8c355d494c05e7fb96b3d498857a
tranducmanh20194617 <102524701+tranducmanh20194617@users.noreply.github.com>
2023-02-09 23:26:33 +0700

    feedback form

---
+++ project/commitform/commenttest.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+ <?php include "../topbar/topbar.php" ?>
+<body class="commentpost">
+	<div class="comment-session">
+		<div class="post-comment">
+			<div class="list">
+				<div class="user">
+					<div class="image"><img src="../images/avatar.jpg" alt="image"></div>
+						<div class="user-meta">
+							<div class="name">namedư</div>
+							<div class="day">3 day ago</div>
+						</div>
+				</div>
+				<div class="comment-post">fafadasda</div>
+			</div>
+		</div>
+		<div class="comment-box">
+			<div class="user">
+				<div class="image"><img src="../images/avatar.jpg" alt=""></div>
+				<div class="name">manhđs</div>
+			</div>
+			<form action="" method="post">
+				<textarea name="comment" id="" cols="30" rows="10" placeholder="Your Comment"></textarea>
+				<a href="../trangchu/foodinfo.php"><button class="comment-submit">Feedback</button></a>
+			</form>
+		</div>
+	</div>
+	 <?php include "../footer/footer.php" ?>
+</body>
+
+</html>
